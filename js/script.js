bd0933c0e76e5eebbb2f1ea36087290ac653bd91
acaba5067 <acaba5067@gmail.com>
2024-08-20 13:56:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const apiKey="bd82ddede9e851e955e220fc526362ba";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const searchBox=document.querySelector(".search input")
+const searchBtn=document.querySelector(".search button")
+const weatherIcon=document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const reponse=await fetch(apiurl+city+`&appid=${apiKey}`)
+    if(reponse.status==404){
+        document.querySelector(".error").style.display="block"
+         document.querySelector(".weather").style.display="none"
+    }else
+    {
+        var data=await reponse.json();
+        // console.log(data)
+         document.querySelector(".city").innerHTML= data.name
+         document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"c"
+         document.querySelector(".humidity").innerHTML= data.main.humidity +"%"
+         document.querySelector(".wind").innerHTML= data.wind.speed+"km/h"
+         if(data.weather[0].main=="Clouds"){
+               weatherIcon.src="images/clouds.png"
+         }
+         else if(data.weather[0].main=="Rain"){
+             weatherIcon.src="images/rain.png"
+         }
+     
+         else if(data.weather[0].main=="Drizzle"){
+             weatherIcon.src="images/drizzle.png"
+         }
+         else if(data.weather[0].main=="Mist"){
+             weatherIcon.src="images/mist.png"
+         }
+         document.querySelector(".weather").style.display="block"
+          document.querySelector(".error").style.display="none"
+     }
+
+    }
+ 
+checkWeather()
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value)
+
+
+})
